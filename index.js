9c1e38629062d60bb3e2f4c8b36592161b7e806c
Sharhan123 <sharhanmohammed03@gmail.com>
2024-03-28 15:39:28 +0530

    error fixing

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const corsOptions = {
-    origin: 'https://dns-manager-9o3d8yuce-sharhan123s-projects.vercel.app', 
-    credentials: true, 
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-};
-
-app.use(cors(corsOptions));
+app.use(cors())
   
   
 
